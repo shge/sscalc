fc1e9b0e37c24d5603296b8034e85acc5dd068d4
shge <25113178+shge@users.noreply.github.com>
2018-04-06 20:29:33 +0900

    Add nav.html, disabled input

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 $(function() {
 
+$('#nav').load('nav.html?201804062022')
+
 // Button
 $('span.button').on('click', function(){
-    $('span.button').removeClass('is-link');
-    $(this).addClass('is-link');
-});
+  $('span.button').removeClass('is-link');
+  $(this).addClass('is-link');
 
-$('span.button').on('click', function(){
-    $('span.button').removeClass('is-link');
-    $(this).addClass('is-link');
+  switch ($(this).text()) {
+    case '標準偏差':
+      $('input').prop("disabled", false);
+      $('#sd').prop("disabled", true);
+    break;
+    case '偏差値':
+      $('input').prop("disabled", false);
+      $('#ss').prop("disabled", true);
+    break;
+    case '平均点':
+      $('input').prop("disabled", false);
+      $('#avg').prop("disabled", true);
+    break;
+    default:
+      $('input').prop("disabled", false);
+      $('#score').prop("disabled", true);
+    break;
+  }
 });
 
 // Calc
@@ -19,18 +35,18 @@ $('input').on('input', function() {
       score = $('#score').val();
 
   switch ($('span.is-link').text()) {
-  case '標準偏差':
-    $('#sd').val( Math.round( (score-avg) * 100 / (ss-50) ) / 10 );
-  break;
-  case '偏差値':
-    $('#ss').val( Math.round( ((score-avg) / sd * 10 + 50) * 10 ) / 10 );
-  break;
-  case '平均点':
-    $('#avg').val( Math.round( 100 * score - (ss-50) * 10 * sd) / 100 );
-  break;
-  default:
-    $('#score').val( Math.round( sd * (ss-50) + avg * 10 ) / 10 );
-  break;
+    case '標準偏差':
+      $('#sd').val( Math.round( (score-avg) * 100 / (ss-50) ) / 10 );
+    break;
+    case '偏差値':
+      $('#ss').val( Math.round( ((score-avg) / sd * 10 + 50) * 10 ) / 10 );
+    break;
+    case '平均点':
+      $('#avg').val( Math.round( 100 * score - (ss-50) * 10 * sd) / 100 );
+    break;
+    default:
+      $('#score').val( Math.round( sd * (ss-50) + avg * 10 ) / 10 );
+    break;
   }
 });
 
